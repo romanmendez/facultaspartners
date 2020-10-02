ab126d1f047b6438b5ecf149f4d05c47ee923493
Román Méndez <mendezbeck@gmail.com>
2020-10-02 09:02:26 +0200

    header done

--- src/layout/Cover.js
+++ src/layout/Cover.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 
 const Cover = styled.div`
+  display: flex;
+  justify-content: center;
   background: url(${props => props.background});
   min-height: ${props => props.height};
   background-size: cover;
